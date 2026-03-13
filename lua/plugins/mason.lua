744f5ef506742328314666df865f58e98a520724
Simon Manzler <simanzler@gmail.com>
2026-03-13 13:18:32 -0400

    added mason with lsp

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+  { 'mason-org/mason.nvim', opts = {} },
+
+  {
+    'mason-org/mason-lspconfig.nvim',
+    dependencies = { 'neovim/nvim-lspconfig' },
+    opts = {
+      ensure_installed = {
+        'vtsls',
+	'lua_ls',
+	'tailwindcss',
+	'eslint'
+    }
+  },
+}
