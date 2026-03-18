f155ef4c9f28e4698192c5c50e9a206df01044ed
Simon Manzler <simanzler@gmail.com>
2026-03-18 14:30:08 -0400

    added harpoon and trouble

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,6 +20,9 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Setup quick sync
+vim.keymap.set("n", "<leader>ls", "<cmd>:Lazy sync<cr>")
 
 -- Setup lazy.nvim
 require("lazy").setup({
