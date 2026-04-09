f39458601e3d38b10e3a08e004391254af4aaf71
Simon Manzler <simanzler@gmail.com>
2026-04-09 09:17:04 -0400

    updated nvim styling

--- xdg/nvim/lua/plugins/catppuccin.lua
+++ xdg/nvim/lua/plugins/catppuccin.lua
@@ -3,7 +3,12 @@ return {
   name = "catppuccin",
   opts = {
     flavour = "mocha",
-    transparent_background = true
+    transparent_background = true,
+    custom_highlights = {
+      NormalFloat = { bg = "none" },
+      TelescopeBorder = { bg = "none" }
+    },
+    auto_integrations = true
   },
   lazy = false,
   config = function(_, opts)
--- xdg/nvim/lua/plugins/lualine.lua
+++ xdg/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local custom_codedark = require('lualine.themes.codedark')
-    custom_codedark.normal.c.bg = 'None'
-
-    require('lualine').setup({
-      options = {
-        theme = custom_codedark
-      },
-      extensions = {
-        'oil'
-      }
-    })
-  end
+  opts = {
+    options = {
+      component_separators = { left = '|', right = '|' },
+      section_separators = { left = '', right = '' },
+    },
+    extensions = {
+      'oil'
+    }
+  }
 }
