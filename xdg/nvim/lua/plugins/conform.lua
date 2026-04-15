a4405e65e7dc3ecef34859b84ad44fce2547ec9b
Simon Manzler <simanzler@gmail.com>
2026-04-15 19:42:03 -0400

    updated some keybinds and added css to conform

--- xdg/nvim/lua/plugins/conform.lua
+++ xdg/nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
       typescript = { "prettierd", "prettier", stop_after_first = true },
       javascriptreact = { "prettierd", "prettier", stop_after_first = true },
       typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
--- xdg/nvim/lua/plugins/oil.lua
+++ xdg/nvim/lua/plugins/oil.lua
@@ -3,7 +3,12 @@ return {
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup({
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false
+      }
+    })
 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end
