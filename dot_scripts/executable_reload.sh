f88f879c8b6164a6f948d327ca0405ce7d392584
Simon Manzler <simanzler@gmail.com>
2026-05-05 07:53:19 -0400

    updated reload script

---
+++ dot_scripts/executable_reload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# chezmoi
+chezmoi apply
+
+# waybar
+killall waybar
+nohup waybar >/dev/null 2>&1 &
+
+# hyprpaper
+killall hyprpaper
+nohup hyprpaper >/dev/null 2>&1 &
+
+# ghostty
+systemctl reload --user app-com.mitchellh.ghostty.service
