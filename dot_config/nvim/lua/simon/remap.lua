6ffd64bf33f639ccbcd33325e56381e4cc095f90
Simon Manzler <simanzler@gmail.com>
2026-05-25 18:51:29 -0400

    added yazi

--- dot_config/nvim/lua/simon/remap.lua
+++ dot_config/nvim/lua/simon/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+vim.keymap.set("n", "<leader>nw", ":noautocmd w<CR>")
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,5 @@
+require("relative-motions"):setup({
+  show_numbers = "relative",
+  show_motion = true,
+  enter_mode = "first"
+})
