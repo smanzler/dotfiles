13ebf2970d72e09c6e857a6d96980af1a9810df7
Simon Manzler <simanzler@gmail.com>
2026-05-31 10:30:04 -0400

    changed to autostart hyprland items

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -40,20 +40,13 @@ local browser     = "firefox"
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function()
+  hl.exec_cmd("waybar & hyprpaper")
+  hl.exec_cmd("sunshine");
 
-hl.exec_cmd("waybar & hyprpaper")
-hl.exec_cmd("sunshine");
-
-hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
-hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "adw-gtw3-dark"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "adw-gtw3-dark"')
+end)
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
